72bd1180ee493b3c90e842430815810a5addb1f2
Muhammad-Taha-Siddiqui <64227124+Muhammad-Taha-Siddiqui@users.noreply.github.com>
2020-04-23 13:50:00 -0700

    Add files via upload
    This is an Assignment...

---
+++ print.py
@@ -0,0 +1,34 @@
+print('"First we do for Float Division"')
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=int(a)/int(b)
+print(c)
+
+### Now for Integer division
+print('"Now we do for Integer Division"')
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=int(a)//int(b)
+print(c)
+
+### Now Multiplication
+print("Now we do for Multiplication")
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=int(a)*int(b)
+print(c)
+
+#### Now Addition
+print('"Now we do for Addition"')
+a=input("Enter first number:")
+b=input("Enter second number:")
+c=int(a)+int(b)
+print(c)
+
+### Now For Substraction
+
+print('"Now we do for Substraction"')
+a=input("Enter first number:")
+b=input("Enter second number:")
+c=int(a)-int(b)
+print(c)
